2890fd778352a12f04c305f8749a99572e1a94de
Aditya <yraditya895@gmail.com>
2026-06-29 14:52:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aditya Y",
+    author_email="yraditya895@gmail.com",
+    packages=find_packages()
+)
