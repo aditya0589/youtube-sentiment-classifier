84607dfc35e54c38df87d00a089d1498d25fccb8
Aditya <yraditya895@gmail.com>
2026-06-30 19:48:11 +0530

    deployment

--- setup.py
+++ setup.py
@@ -5,5 +5,17 @@ setup(
     version="0.0.1",
     author="Aditya Y",
     author_email="yraditya895@gmail.com",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "google-api-python-client",
+        "nltk",
+        "python-dotenv",
+        "from_root",
+        "langdetect"
+    ]
 )
